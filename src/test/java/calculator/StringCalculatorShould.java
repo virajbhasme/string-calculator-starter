30dc7f86225f31387481c33a155a0b73dbaf56ce
virajbhasme <virajbhasme@gmail.com>
2021-10-17 12:15:58 +0530

    test case 6 completed

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -59,4 +59,9 @@ class StringCalculatorShould {
 			assertEquals(e.getMessage(), "Negatives not allowed: -4,-5");
 		}
     }
+    
+    @Test
+    public void testOverThousand(){
+    	assertEquals(2, Calculator.add("1000,2"));
+    }
 }
