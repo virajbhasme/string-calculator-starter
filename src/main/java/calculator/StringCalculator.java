f6c523434432abe06ec4c08ea61fbe85ee871642
virajbhasme <virajbhasme@gmail.com>
2021-10-17 11:46:56 +0530

    testcase 1 completed

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,9 +1,21 @@
 package calculator;
 
 class StringCalculator {
-
-    public int add(String input) {
-        return 0;
-    }
+    
+    public static int add(String text){
+		if(text.equals("")){
+			return 0;
+		}
+		else{
+			String delimiter = ",";
+			if(text.matches("//(.*)\n(.*)")){
+				delimiter = Character.toString(text.charAt(2));
+				text = text.substring(4);
+			}
+			
+			String numList[] = splitNumbers(text, delimiter + "|\n");
+			return sum(numList);
+		}
+	}
 
 }
