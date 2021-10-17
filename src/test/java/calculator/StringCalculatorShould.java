08718545b61bfcffe775ea6d0cc1bead79f3b4ca
virajbhasme <virajbhasme@gmail.com>
2021-10-17 11:43:21 +0530

    test case 1 completed

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -17,4 +17,14 @@ class StringCalculatorShould {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
     }
+    
+    @Test
+	public void testEmptyString() {
+		assertEquals(0, Calculator.add(""));
+	}
+    
+    @Test
+	public void testOneNumber() {
+		assertEquals(1, Calculator.add("1"));
+	}
 }
