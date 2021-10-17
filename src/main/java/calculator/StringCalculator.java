5ccbc39512abe261ce4721c05575c9bdbf3b7e91
virajbhasme <virajbhasme@gmail.com>
2021-10-17 12:13:12 +0530

    testcase 4 completed

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -25,5 +25,27 @@ class StringCalculator {
     private static String[] splitNumbers(String numbers, String divider){
 	    return numbers.split(divider);
 	}
+    
+    private static int sum(String[] numbers){
+ 	    int total = 0;
+ 	    String negString = "";
+
+        for(String number : numbers){
+        	if(toInt(number) < 0){
+        		if(negString.equals(""))
+        			negString = number;
+        		else
+        			negString += ("," + number);
+        	}
+        	if(toInt(number) < 1000)
+		    	total += toInt(number);
+		}
+
+		if(!negString.equals("")){
+			throw new IllegalArgumentException("Negatives not allowed: " + negString);
+		}
+
+		return total;
+    }
 
 }
